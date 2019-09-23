e3a4f8640445322701530060eb2a2a376088964b
Gerardo <geracs01@gmail.com>
2019-09-22 20:54:34 -0500

    DebouncerFinal

--- MKL27Z64xxx4_Project/source/Driver_Debouncer.c
+++ MKL27Z64xxx4_Project/source/Driver_Debouncer.c
@@ -19,50 +19,50 @@
 /****** Global Variables *******/
 
 
- uint8_t u8IndexButton;
+ uint32_t u8IndexButton;
 
 
 /****** Local Variables *******/
 
- static uint8_t u8ReadValue[2];
- static uint8_t u8Samples[2][3];
- static uint8_t u8Index[2];
- static uint8_t u8StableValue[2];
+ static uint32_t u32ReadValue[6];
+ static uint32_t u32Samples[6][3];
+ static uint32_t u32Index[6];
+ static uint32_t u32StableValue[6];
 
 
 /****** Local Functions *******/
  static uint32_t dwf_Debouncer(GPIO_Type *bases, uint32_t u32pins)
  	{
 
- 	u8ReadValue[u8IndexButton] = (((bases->PDIR) >> u32pins) & 0x01U);
- 		if(u8ReadValue[u8IndexButton] != u8StableValue[u8IndexButton])
+ 	u32ReadValue[u8IndexButton] = (((bases->PDIR) >> u32pins) & 0x01U);
+ 		if(u32ReadValue[u8IndexButton] != u32StableValue[u8IndexButton])
  		{
- 			u8Samples[u8IndexButton][u8Index[Zero]]= u8ReadValue[u8IndexButton];
+ 			u32Samples[u32IndexButton][u32Index[Zero]]= u32ReadValue[u32IndexButton];
 
- 			u8Index[u8IndexButton] ++;
+ 			u32Index[u32IndexButton] ++;
  		}else
  		{
- 			u8Index[u8IndexButton] =Zero;
+ 			u32Index[u32IndexButton] =Zero;
 
  		}
- 		if(u8Samples[u8IndexButton][FirstSample]  == u8Samples[u8IndexButton][ThirdSample])
+ 		if(u32Samples[u32IndexButton][FirstSample]  == u32Samples[u32IndexButton][ThirdSample])
  		{
- 			u8StableValue[u8IndexButton] = u8Samples[u8IndexButton][FirstSample];
- 			u8Index[u8IndexButton] =Zero;
+ 			u32StableValue[u32IndexButton] = u32Samples[u32IndexButton][FirstSample];
+ 			u32Index[u32IndexButton] =Zero;
  		}
- 		else if(u8Samples[u8IndexButton][FirstSample] != u8Samples[u8IndexButton][ThirdSample])
+ 		else if(u32Samples[u32IndexButton][FirstSample] != u32Samples[u32IndexButton][ThirdSample])
  		{
- 			u8StableValue[u8IndexButton] = u8Samples[u8IndexButton][FirstSample];
- 			u8Index[u8IndexButton] =Zero;
+ 			u32StableValue[u32IndexButton] = u32Samples[u32IndexButton][FirstSample];
+ 			u32Index[u32IndexButton] =Zero;
  		}
 
- 		if(u8ReadValue[u8IndexButton] == u8StableValue[u8IndexButton]){
- 				u8StableValue[u8IndexButton] = Zero;
+ 		if(u32ReadValue[u32IndexButton] == u32StableValue[u32IndexButton]){
+ 				u32StableValue[u32IndexButton] = Zero;
  			}
 
 
 
- 		return u8StableValue[u8IndexButton];
+ 		return u32StableValue[u32IndexButton];
 
  	}
 
@@ -75,41 +75,41 @@
 
  uint32_t Bnf_CheckUpButton(void)
  {
- 	u8IndexButton =0;
-  return dwf_Debouncer(GPIOD,7);
+ 	u32IndexButton =0;
+  return dwf_Debouncer(GPIOA,1);
 
  }
 
  uint32_t Bnf_CheckDownButton(void)
  {
- 	u8IndexButton =0;
-  return dwf_Debouncer(GPIOD,7);
+ 	u32IndexButton =1;
+  return dwf_Debouncer(GPIOA,2);
 
  }
 
  uint32_t Bnf_CheckRigthButton(void)
  {
- 	u8IndexButton =0;
-  return dwf_Debouncer(GPIOD,7);
+ 	u32IndexButton =2;
+  return dwf_Debouncer(GPIOE,25);
 
  }
 
  uint32_t Bnf_CheckLeftButton(void)
   {
-  	u8IndexButton =0;
-   return dwf_Debouncer(GPIOD,7);
+  	u8IndexButton =3;
+   return dwf_Debouncer(GPIOA,4);
 
   }
  uint32_t Bnf_CheckRunButton(void)
    {
-   	u8IndexButton =0;
-    return dwf_Debouncer(GPIOD,7);
+   	u8IndexButton =4;
+    return dwf_Debouncer(GPIOA,12);
 
    }
  uint32_t Bnf_CheckConfigButton(void)
    {
-   	u8IndexButton =0;
-    return dwf_Debouncer(GPIOD,7);
+   	u8IndexButton =5;
+    return dwf_Debouncer(GPIOA,13);
 
    }
 
